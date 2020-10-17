566f29a76211477749a81908037cf773b5efbbcb
Suresh Yhap <sureshyhap@gmail.com>
2020-10-17 07:15:28 -0400

    Project Euler 16 and str_replace

--- Algorithms/implement_str_replace/implement_str_replace.cpp
+++ Algorithms/implement_str_replace/implement_str_replace.cpp
@@ -4,8 +4,8 @@
 void replace(char*& cs, int start, int len, const char* to_splice);
 
 int main(int argc, char* argv[]) {
-  char* cs = new char[6 + 1];
-  strcpy(cs, "Hello World!");
+  char* cs = new char[12 + 1];
+  strcpy(cs, "Hello World!abcedef");
   std::cout << cs << std::endl;
   replace(cs, 6, 5, "Planet");
   std::cout << cs << std::endl;
@@ -13,15 +13,37 @@ int main(int argc, char* argv[]) {
   std::cout << cs << std::endl;
   replace(cs, 6, 2, "By");
   std::cout << cs << std::endl;
+  delete [] cs;
   return 0;
 }
 
 void replace(char*& cs, int start, int len, const char* to_splice) {
   int len_to_splice = strlen(to_splice);
   int len_original = strlen(cs);
+  if (start + len > len_original) {
+    return;
+  }
   int diff = len_to_splice - len;
-  if (len_to_splice != len) {
-    char* temp = new char[len_original + diff + 1];
+  int len_temp = len_original + diff;
+  char* temp = new char[len_temp + 1];
+  if (diff < 0) {
+    for (int i = 0; i < start; ++i) {
+      temp[i] = cs[i];
+    }
+    for (int i = 0; i < len_to_splice; ++i) {
+      temp[start + i] = to_splice[i];
+    }
+    for (int i = start; i < len_temp; ++i) {
+      temp[i + len_to_splice] = cs[i + len];
+    }
+    temp[len_temp] = '\0';
+    delete [] cs;
+    cs = new char[len_temp + 1];
+    strcpy(cs, temp);
+    delete [] temp;
+    return;
+  }
+  else if (len_to_splice != len) {
     for (int i = 0; i < start; ++i) {
       temp[i] = cs[i];
     }
@@ -31,16 +53,15 @@ void replace(char*& cs, int start, int len, const char* to_splice) {
     for (int i = start + len; i < len_original; ++i) {
       temp[i + diff] = cs[i];
     }
-    int len_temp = len_original + diff;
     temp[len_temp] = '\0';
-    delete cs;
+    delete [] cs;
     cs = new char[len_temp + 1];
     strcpy(cs, temp);
-    delete temp;
+    delete [] temp;
   }
   else {
     for (int i = 0; i < len_to_splice; ++i) {
-      cs[start + 1] = to_splice[i];
+      cs[start + i] = to_splice[i];
     }
   }
 }
